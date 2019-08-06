a174b7e7e59e4b77e19f342ee5d39c2fb6d7e406
AnastasiaBro <a-sirion@yandex.ru>
2019-08-06 15:12:12 +0300

    Update London.js

--- src/components/London/London.js
+++ src/components/London/London.js
@@ -4,7 +4,8 @@ const london = (props) => {
     return (
         <Article>
             <div className="note__first-row">
-                Путешествие в Таллинн!
+                <h2 className="note__title">Путешествие в Таллинн!</h2>
+                <p>6 августа 2019</p>
             </div>
         <Article/>
     )
